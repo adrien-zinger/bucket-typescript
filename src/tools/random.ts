5d189b26501af23cdb3329f5afd8bc99e6aa822a
zinger adrien <zinger.ad>
2020-06-05 15:02:40 +0200

    add shuffle method in my random lib

--- src/tools/random.ts
+++ src/tools/random.ts
@@ -9,4 +9,17 @@ export default class Random {
         max = Math.floor(max);
         return Math.floor(Math.random() * (max - min + 1)) + min;
     }
+
+    public static shuffle(array: any[]): void {
+        let currentIndex: number = array.length;
+        let tmpValue: number;
+        let randomIndex: number;
+        while (0 !== currentIndex) {
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            tmpValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = tmpValue;
+        }
+    }
 } 
