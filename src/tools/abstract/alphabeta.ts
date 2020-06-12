d4fec50a49433f87f6ed555173b510023d289b2a
zinger adrien <zinger.ad>
2020-06-12 11:41:10 +0200

    Just minor modifications

--- src/tools/abstract/alphabeta.ts
+++ src/tools/abstract/alphabeta.ts
@@ -1,13 +1,14 @@
-export abstract class AlphaBetaNode {
-    min: boolean = false;
+export abstract class Node {
     abstract set value(v: number);
     abstract get value(): number;
     abstract get id(): string;
 }
 
-export abstract class AlphaBeta<T extends AlphaBetaNode> {
+export abstract class AlphaBeta<T extends Node> {
     constructor(private maxHeight: number = Infinity) {}
 
+    protected log: (...args: any) => void = () => {};
+ 
     /**
      * Returns an evaluation of a node between 0 and Infinity
      */
@@ -24,43 +25,40 @@ export abstract class AlphaBeta<T extends AlphaBetaNode> {
     protected abstract getNexts(node: T): T[];
 
     /**
-     * Classical alphabeta algorithm that can be generalized to graph, tree or everything
-     * that implement this class
-     * 
-     * The vertices can be classicly min|max but you can use this algorithm with only max values.
-     * Or if you prefer you can just reimplement what you want.
+     * Classical alphabeta pruning algorithm that can be generalized to graph,
+     * tree or everything that implement this class
      * 
      * @param node Head node of the tree
      * @param alpha Initialized to -Infinity
      * @param beta Initialized to Infinity
      * @param height Height Deep of the search
      */
-    public run(node: T, alpha: number = Number.NEGATIVE_INFINITY,
+    public run(node: T, max: boolean = true, alpha: number = Number.NEGATIVE_INFINITY,
         beta: number = Number.POSITIVE_INFINITY, height: number = 0): number {
         if (this.isLeaf(node) || height >= this.maxHeight) {
             return this.evaluation(node);
         }
-        if (node.min) {
-            node.value = Number.POSITIVE_INFINITY;
-            for (let child of this.getNexts(node)) {
-                node.value = Math.min(node.value,
-                    this.run(child, alpha, beta, height + 1));
-                if (alpha >= node.value) {
-                    //console.log("min | cut", vertex.id, vertex.value);
-                    return node.value;
-                }
-                beta = Math.min(beta, node.value);
-            }
-        } else {
+        if (max) {
             node.value = Number.NEGATIVE_INFINITY;
             for (let child of this.getNexts(node)) {
                 node.value = Math.max(node.value,
-                    this.run(child, alpha, beta, height + 1));        
+                    this.run(child, false, alpha, beta, height + 1));        
                 if (node.value >= beta) {
-                    //console.log("max | cut", vertex.id, vertex.value);
+                    this.log("max | cut", child.id, child.value);
                     return node.value;
                 }
                 alpha = Math.max(alpha, node.value);
+            }
+        } else {
+            node.value = Number.POSITIVE_INFINITY;
+            for (let child of this.getNexts(node)) {
+                node.value = Math.min(node.value,
+                    this.run(child, true, alpha, beta, height + 1));
+                if (alpha >= node.value) {
+                    this.log("min | cut", child.id, child.value);
+                    return node.value;
+                }
+                beta = Math.min(beta, node.value);
             }
         }
         return node.value;
